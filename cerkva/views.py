6e2e9cdc38c0ef9ded82aac6e58ee639dcd335ef
OrestSkorokhod <orestskorohod@gmail.com>
2019-05-06 18:46:48 +0300

    Added possibility to do messages, added pagination

--- cerkva/views.py
+++ cerkva/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from django.urls import reverse
-
+from django.core.paginator import Paginator
 
 from religion.forms import MessageForm
 from religion.models import *
@@ -27,6 +27,41 @@ def order_service(request):
 
 @login_required
 def messages_list(request):
+    on_page = 3
+    text = None
+    if request.method == 'POST':
+        id = request.POST.get('id')
+        message = Message.objects.filter(id=id).first()
+        # print(message.is_active)
+        message.is_active = False
+        message.save()
+        text = 'Ви виконали прохання "' + message.service.name + '" для ' + message.user
+
     messages = Message.objects.filter(is_active=True)
 
-    return render(request, 'messages_list.html', context={'messages': messages})
+    paginator = Paginator(messages, on_page)
+    page_number = request.GET.get('page', 1)
+    page = paginator.get_page(page_number)
+
+    is_paginated = page.has_other_pages()
+
+    if page.has_previous():
+        prev_url = '?page={}'.format(page.previous_page_number())
+    else:
+        prev_url = ''
+
+    if page.has_next():
+        next_url = '?page={}'.format(page.next_page_number())
+    else:
+        next_url = ''
+
+    context = {
+        'page': page,
+        'is_paginated': is_paginated,
+        'next_url': next_url,
+        'prev_url': prev_url,
+        'text': text,
+
+    }
+
+    return render(request, 'messages_list.html', context=context)
